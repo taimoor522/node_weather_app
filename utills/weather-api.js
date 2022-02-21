40ef4ec5e216f3367702924292ee23b6810e0ca8
Taimoor Ghafar <khantaimoor522@gmail.com>
2022-02-21 17:08:41 +0500

    remove utills folder

--- utills/weather-api.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios").default;
-
-const api_key = "150e1f904ffc6f8fbccb17e44bc6e76f";
-
-function getWeather(location) {
-  axios.get(
-    `api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${api_key}`
-  );
-}
